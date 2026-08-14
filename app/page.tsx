357f1cd99cd641a4593d2669d599d1458c9d58e7
Mr Nithin <nmnsnithinsai6675@gmail.com>
2026-08-14 16:05:33 +0530

    Remove duplicate homepage navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Features from "../components/Features";
 import Stats from "../components/Stats";
@@ -7,8 +6,6 @@ import Footer from "../components/Footer";
 export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-br from-blue-100 via-white to-indigo-100">
-
-      <Navbar />
 
       <Hero />
 
